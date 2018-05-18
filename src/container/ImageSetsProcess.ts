0e78b4d6439b32d8fae18b30ecde6032aaf15b1f
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-18 14:12:15 +0300

    added new build structure

--- src/container/ImageSetsProcess.ts
+++ src/container/ImageSetsProcess.ts
@@ -1,5 +1,7 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultNetFactory } from 'pip-services-net-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { ImageSetsServiceFactory } from '../build/ImageSetsServiceFactory';
 
@@ -8,6 +10,8 @@ export class ImageSetsProcess extends ProcessContainer {
     public constructor() {
         super("imagesets", "Image library microservice");
         this._factories.add(new ImageSetsServiceFactory);
+        this._factories.add(new DefaultNetFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 
 }
