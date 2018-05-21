7e69e6223b211698075063a409873fa23648c989
pipdeveloper <pipdevs@gmail.com>
2018-05-21 13:17:46 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/ImageSetsHttpServiceV1.js
+++ obj/src/services/version1/ImageSetsHttpServiceV1.js
@@ -4,7 +4,7 @@ const pip_services_commons_node_1 = require("pip-services-commons-node");
 const pip_services_net_node_1 = require("pip-services-net-node");
 class ImageSetsHttpServiceV1 extends pip_services_net_node_1.CommandableHttpService {
     constructor() {
-        super('imagesets');
+        super('v1/imagesets');
         this._dependencyResolver.put('controller', new pip_services_commons_node_1.Descriptor('pip-services-imagesets', 'controller', 'default', '*', '1.0'));
     }
 }
