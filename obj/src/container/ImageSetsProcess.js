710163faec8606b46dc48159324f95514210c3b5
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 14:06:15 +0300

    added DefaultRpcFactory

--- obj/src/container/ImageSetsProcess.js
+++ obj/src/container/ImageSetsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const ImageSetsServiceFactory_1 = require("../build/ImageSetsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class ImageSetsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("imagesets", "Image library microservice");
         this._factories.add(new ImageSetsServiceFactory_1.ImageSetsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.ImageSetsProcess = ImageSetsProcess;
--- src/container/ImageSetsProcess.ts
+++ src/container/ImageSetsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { ImageSetsServiceFactory } from '../build/ImageSetsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class ImageSetsProcess extends ProcessContainer {
 
     public constructor() {
         super("imagesets", "Image library microservice");
         this._factories.add(new ImageSetsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
