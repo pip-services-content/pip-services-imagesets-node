8ea5ac5f5d39b3b3ff5313d63126147345eac5b2
pipdeveloper <pipdevs@gmail.com>
2019-04-23 12:55:26 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/ImageSetsMongoDbPersistence.d.ts
+++ obj/src/persistence/ImageSetsMongoDbPersistence.d.ts
@@ -5,6 +5,6 @@ import { ImageSetV1 } from '../data/version1/ImageSetV1';
 import { IImageSetsPersistence } from './IImageSetsPersistence';
 export declare class ImageSetsMongoDbPersistence extends IdentifiableMongoDbPersistence<ImageSetV1, string> implements IImageSetsPersistence {
     constructor();
-    private composeFilter;
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: any): void;
 }
